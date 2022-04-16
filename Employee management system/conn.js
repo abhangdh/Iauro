ea56495bc1fccdfeaa1b3b948c80a625da24e86f
abhangdh <abhangdh@gmail.com>
2022-04-16 15:50:56 +0530

    Docker_assignment

--- Employee management system/conn.js
+++ Employee management system/conn.js
@@ -1,18 +1,18 @@
-var mysql = require('mysql');
+// var mysql = require('mysql');
 
-var db_con = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "employee"
-});
+// var db_con = mysql.createConnection({
+//   host: "localhost",
+//   user: "root",
+//   password: "",
+//   database: "employee"
+// });
 
-db_con.connect((err) => {
-    if (err) {
-      console.log("Database Connection Failed !!!", err);
-    } else {
-      console.log("connected to Database");
-    }
-  });
+// db_con.connect((err) => {
+//     if (err) {
+//       console.log("Database Connection Failed !!!", err);
+//     } else {
+//       console.log("connected to Database");
+//     }
+//   });
     
-  module.exports = db_con;
+//   module.exports = db_con;
