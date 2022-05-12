9fd039b727173a7e97b1fea91575337671d55f55
abhangdh <abhangdh@gmail.com>
2022-05-12 17:39:27 +0530

    Angular_assignment

--- Emp_Angular_Assignment/src/app/app.component.spec.ts
+++ Emp_Angular_Assignment/src/app/app.component.spec.ts
@@ -1,9 +1,13 @@
 import { TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule
+      ],
       declarations: [
         AppComponent
       ],
@@ -16,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'blog'`, () => {
+  it(`should have as title 'Emp_Angular_Assignment'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('blog');
+    expect(app.title).toEqual('Emp_Angular_Assignment');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('blog app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('Emp_Angular_Assignment app is running!');
   });
 });
