fcc5d65a749ebbd569f0dcc05e63173e11b980b0
abhangdh <abhangdh@gmail.com>
2022-07-05 14:56:32 +0530

    angular custom_pipes

--- Angular_Assignment/src/app/app-routing.module.ts
+++ Angular_Assignment/src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EmployeeComponent } from './employee/employee.component';
+import { HeaderComponent } from './header/header.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'header',component: HeaderComponent},
+  { path: 'employee', component: EmployeeComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
+export const routingComponents = [HeaderComponent, EmployeeComponent ]
